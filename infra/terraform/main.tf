a294a2d7bda7a898c7787967c847aa6722c6f296
Bharathi Raja <bharujar@gmail.com>
2026-08-18 15:53:21 +0200

    # Added KeyVault Components and It's associated resources

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -116,3 +116,23 @@ module "storage" {
   enable_diagnostics             = true
   log_analytics_workspace_id     = ""
 }
+
+# -----------------------------------------------------------------------------
+# Keyvault Components — + Private Endpoint + Private DNS Zone Link
+# -----------------------------------------------------------------------------
+
+module "key_vault" {
+  source = "../modules/key-vault"
+
+  name                          = "kv-${var.environment}-${var.name_prefix}"
+  resource_group_name           = azurerm_resource_group.rg_infra.name
+  location                      = azurerm_resource_group.rg_infra.location
+  tags                          = azurerm_resource_group.rg_infra.tags
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  private_endpoint_subnet_id    = module.network_spoke.private_endpoint_subnet_id
+  private_dns_zone_id           = module.private_dns.zone_ids["privatelink.vaultcore.azure.net"]
+  secrets_user_principal_ids    = ""
+  secrets_officer_principal_ids = ""
+  enable_diagnostics            = true
+  log_analytics_workspace_id    = ""
+}
