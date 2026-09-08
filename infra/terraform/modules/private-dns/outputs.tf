90a7699bdddfe2de28c8c43be488e65723fc07e0
Bharathi Raja <bharujar@gmail.com>
2026-09-08 15:18:09 +0200

    updated

--- infra/terraform/modules/private-dns/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "zone_ids" {
-  description = "Map of zone name to resource ID."
-  value       = { for name, zone in azurerm_private_dns_zone.dns : name => zone.id }
-}
-
-output "zone_names" {
-  description = "Map of zone name to zone name."
-  value       = { for name, zone in azurerm_private_dns_zone.dns : name => zone.name }
-}
